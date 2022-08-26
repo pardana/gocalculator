97dd4a86005fa9ea3910656ebb9e318fedaacea9
Aditria Pardana <aditria.pardana20@gmail.com>
2022-08-26 14:19:32 +0700

    update component button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Button from "./components/Button";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      current: "0",
+      previous: [],
+    };
+  }
+
+  reset = () => {
+    this.setState({ result: "0" });
+  };
+
+  addToCurrent = (symbol) => {
+    this.setState({ current: this.state.current + symbol });
+  };
+
+  render() {
+    const buttons = [
+      { symbol: "C", cols: 3, action: this.reset },
+      { symbol: "/", cols: 1, action: this.addToCurrent },
+    ];
+
+    return (
+      <div className="App">
+        <input className="result" type="text" value={this.state.current} />
+        <Button />
+      </div>
+    );
+  }
 }
 
 export default App;
