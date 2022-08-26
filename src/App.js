314f9bf03a2b8783adee65c4fcade4135b9be478
Aditria Pardana <aditria.pardana20@gmail.com>
2022-08-26 15:56:38 +0700

    update function calc

--- src/App.js
+++ src/App.js
@@ -9,27 +9,39 @@ class App extends Component {
     this.state = {
       current: "0",
       previous: [],
-      nextIsReset: false
+      nextIsReset: false,
     };
   }
 
   reset = () => {
-    this.setState({ result: "0" });
+    this.setState({ current: "0", previous: [], nextIsReset: false });
   };
 
   addToCurrent = (symbol) => {
     console.log("addToCurrent");
 
-    if (["/", "-", "+", "x"].indexOf(symbol) > -1) {
+    if (["/", "-", "+", "*"].indexOf(symbol) > -1) {
       let { previous } = this.state;
       previous.push(this.state.current + symbol);
       this.setState({ previous: previous, nextIsReset: true });
     } else {
-      if ((this.state.current === "0" && symbol !== "." || this.state.nextIsReset)) {
+      if (
+        (this.state.current === "0" && symbol !== ".") ||
+        this.state.nextIsReset
+      ) {
         this.setState({ current: symbol, nextIsReset: false });
       } else {
         this.setState({ current: this.state.current + symbol });
       }
+    }
+  };
+
+  calculate = (symbol) => {
+    let { current, previous, nextIsReset } = this.state;
+
+    if (previous.length > 0) {
+      current = eval(String(previous[previous.length - 1] + current));
+      this.setState({ current, previous: [], nextIsReset: true });
     }
   };
 
@@ -40,7 +52,7 @@ class App extends Component {
       { symbol: "7", cols: 1, action: this.addToCurrent },
       { symbol: "8", cols: 1, action: this.addToCurrent },
       { symbol: "9", cols: 1, action: this.addToCurrent },
-      { symbol: "x", cols: 1, action: this.addToCurrent },
+      { symbol: "*", cols: 1, action: this.addToCurrent },
       { symbol: "4", cols: 1, action: this.addToCurrent },
       { symbol: "5", cols: 1, action: this.addToCurrent },
       { symbol: "6", cols: 1, action: this.addToCurrent },
@@ -51,7 +63,7 @@ class App extends Component {
       { symbol: "+", cols: 1, action: this.addToCurrent },
       { symbol: "0", cols: 2, action: this.addToCurrent },
       { symbol: ".", cols: 1, action: this.addToCurrent },
-      { symbol: "=", cols: 1, action: this.addToCurrent },
+      { symbol: "=", cols: 1, action: this.calculate },
     ];
 
     return (
